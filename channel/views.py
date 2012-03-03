3b92bb37febf19e2a980f72c73eb04b785776e47
Alden Keefe Sampson <aldenkeefesampson@gmail.com>
2012-03-03 15:54:07 -0500

    need to pull

--- channel/views.py
+++ channel/views.py
@@ -1,4 +1,5 @@
 from django.contrib.auth.decorators import login_required
+from django.core.exceptions import MultipleObjectsReturned
 from django.http import HttpResponseRedirect
 from django.shortcuts import render_to_response
 from django.template.context import RequestContext
@@ -7,7 +8,6 @@ import redis
 import json
 from channel.models import Post, Channel
 
-
 @login_required
 def show (request, channel_name):
     try:
@@ -15,9 +15,17 @@ def show (request, channel_name):
     except MultipleObjectsReturned:
         return render_to_response('error.html',
                 {'error', 'multiple channels with name' + channel_name})
-    posts = Post.objects.filter(channel__name=channel_name)
+    posts = Post.objects.filter(channel=channel.id)
     return render_to_response('channel.html',
                 {'posts': posts, 'channel': channel_name})
+
 @login_required
 def new_post (request, channel_name):
-    return render_to_response('home.html')
+    try:
+        channel = Channel.objects.get(name=channel_name)
+    except MultipleObjectsReturned:
+        return render_to_response('error.html',
+                {'error', 'multiple channels with name' + channel_name})
+
+
+
