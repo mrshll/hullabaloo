6c8054d8693c897d27308c1946f0cf9344833d00
Alden Keefe Sampson <aldenkeefesampson@gmail.com>
2012-03-03 15:16:45 -0500

    added git ignore

--- channel/views.py
+++ channel/views.py
@@ -5,15 +5,19 @@ from django.template.context import RequestContext
 
 import redis
 import json
-from channel.models import Post
+from channel.models import Post, Channel
 
 
 @login_required
 def show (request, channel_name):
+    try:
+        channel = Channel.objects.get(name=channel_name)
+    except MultipleObjectsReturned:
+        return render_to_response('error.html',
+                {'error', 'multiple channels with name' + channel_name})
     posts = Post.objects.filter(channel__name=channel_name)
-    return render_to_response('channel.html', {'posts': posts,
-                                               'channel': channel_name})
-
+    return render_to_response('channel.html',
+                {'posts': posts, 'channel': channel_name})
 @login_required
 def new_post (request, channel_name):
     return render_to_response('home.html')
