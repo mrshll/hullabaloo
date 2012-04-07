9914df7abc6874087907f24b791a47ef22c58467
Marshall Moutenot <mmoutenot@gmail.com>
2012-04-07 15:53:30 -0400

    1. added views to channel model to record recent views

--- channel/models.py
+++ channel/models.py
@@ -1,16 +1,20 @@
 from django.db import models
 from hullabaloo.settings import STATIC_URL
 
-
 class Channel(models.Model):
-    name = models.CharField(max_length=50, verbose_name='channel name')
-
+    name  = models.CharField(max_length=50, verbose_name='channel name')
     def __unicode__(self):
         return self.name
 
+class View(models.Model):
+    channel = models.ForeignKey(Channel)
+    stamp = models.DateTimeField()
+
 class Post(models.Model):
-    image = models.ImageField(upload_to=(STATIC_URL+"user/images/"), height_field=None, width_field=None, max_length=180, null=True, blank=True)
-    body = models.TextField(max_length=500, verbose_name='posts body')
+    image   = models.ImageField(upload_to=(STATIC_URL+"user/images/"),
+                                height_field=None, width_field=None,
+                                max_length=180, null=True, blank=True)
+    body    = models.TextField(max_length=500, verbose_name='posts body')
     channel = models.ForeignKey(Channel)
 
     def __unicode__(self):
